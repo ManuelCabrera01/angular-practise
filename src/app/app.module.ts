d98c071616c1b6cd682b327c8ecf5c5a8b337d86
Manuel <m.a.c.p90@gmail.com>
2017-11-17 12:36:52 -0500

    error at css ? hahahaha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,13 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DirectoryComponent } from './directory/directory.component';
+import {  RouterModule, Routes } from "@angular/router";
 
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',  component: HomeComponent  },
+  { path: 'about', component: DirectoryComponent }
+];
 
 @NgModule({
   declarations: [
@@ -19,6 +25,7 @@ import { DirectoryComponent } from './directory/directory.component';
     BrowserModule,
     FormsModule,      // <-- Here is why ngModel is recognized
     HttpModule,
+     RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
