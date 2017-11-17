2aa7d11464c3ca56137b755c4bfd851d102c01ae
Manuel <m.a.c.p90@gmail.com>
2017-11-17 10:35:45 -0500

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,20 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { DirectoryComponent } from './directory/directory.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
      HomeComponent,
+     DirectoryComponent,
 
   ],
   imports: [
     BrowserModule,
     FormsModule,      // <-- Here is why ngModel is recognized
-    HttpModule
+    HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
